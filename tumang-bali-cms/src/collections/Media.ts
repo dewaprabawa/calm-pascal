35d5382c87278a10eeaaf5d478953c012d611a30
dewaprabawa <balipastika@gmail.com>
2026-06-28 12:11:50 +0800

    fix: explicitly disable local storage on Media collection for vercel

--- tumang-bali-cms/src/collections/Media.ts
+++ tumang-bali-cms/src/collections/Media.ts
@@ -3,7 +3,7 @@ import { CollectionConfig } from 'payload'
 export const Media: CollectionConfig = {
   slug: 'media',
   upload: {
-    staticDir: 'media',
+    disableLocalStorage: true,
     imageSizes: [
       {
         name: 'thumbnail',
