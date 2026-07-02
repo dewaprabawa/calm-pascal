102fa4c175df51b294f159a322257ad5ded634a3
dewaprabawa <balipastika@gmail.com>
2026-07-02 22:12:19 +0800

    fix(vercel-blob): clean double quotes from BLOB_READ_WRITE_TOKEN

--- tumang-bali-cms/src/payload.config.ts
+++ tumang-bali-cms/src/payload.config.ts
@@ -43,7 +43,7 @@ export default buildConfig({
       collections: {
         media: true,
       },
-      token: process.env.BLOB_READ_WRITE_TOKEN || '',
+      token: (process.env.BLOB_READ_WRITE_TOKEN || '').replace(/"/g, ''),
     }),
   ],
   admin: {
